93ad498d3c22544163e568b7684d9452ca87bbed
poyrazn <poyrazf@mef.edu.tr>
2018-03-18 18:36:47 +0300

    Initial Commit

---
+++ OS_Assignment2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OS_Assignment2
+//
+//  Created by Nehir Poyraz on 18.03.2018.
+//  Copyright © 2018 Nehir Poyraz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
